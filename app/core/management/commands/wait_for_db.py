92afcd39d9f2fd1575d1d81d30138f3bce1f1a99
Rahul <rahulmishrabsa38@gmail.com>
2024-04-27 11:28:32 +0530

    added git hub actions

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,17 +7,19 @@ import time
 
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
+        self.stdout.write("checking db availability.......")
         db_up = False
 
         while db_up is False:
             try:
                 self.check(databases = ['default'])
                 db_up = True
-            except:
+                
+            except(Psycopg2OpError,OperationalError):
                 self.stdout.write("waiting for 1 sec to start the db ...")
                 time.sleep(1)
         
-        self.style.SUCCESS("db srarted!...")
+        self.stdout.write(self.style.SUCCESS("db srarted!..."))
 
         
         
